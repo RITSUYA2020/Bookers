f20ac95445cbd192c8e94dff0429520b2e610ccf
RITSUYA2020 <12ritsuya23@gmail.com>
2020-05-19 20:22:44 +0900

    [Update]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
 
   	def create
-  		book = Book.new(book_params)
-  		book.save
-  		redirect_to book_path(book.id)
+  		@book = Book.new(book_params)
+  		if @book.save
+  			flash[:notice] = "Book was successfully created."
+  			redirect_to book_path(@book.id)
+  		else
+  			#indexページの表示処理
+  			@books = Book.all
+  			render :index
+  		end
   	end
 
+	#indexページの表示処理
   	def index
 		@books = Book.all
 		@book = Book.new
@@ -20,9 +27,13 @@ class BooksController < ApplicationController
 	end
 
 	def update
-		book = Book.find(params[:id])
-		book.update(book_params)
-		redirect_to book_path(book.id)
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:notice] = "Book was successfully created."
+			redirect_to book_path(@book.id)
+		else
+			render :edit
+		end
 	end
 
 	def destroy
